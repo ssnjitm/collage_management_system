7fe414830946d59259577772cdb1b962966d8ffa
Jyoti-Adhikari <jyotiadhikari770@gmail.com>
2025-02-01 16:06:30 +0545

    img problem solved

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Set the views directory (optional, only needed if your views are in a custom folder)
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
 
+// Route to render EJS file
 app.get("/", (req, res) => {
-    res.render("./layouts/boilerplate.ejs");
-})
+    res.render("layouts/boilerplate"); // No need for ".ejs"
+});
+
+// Start the server
 app.listen(8080, () => {
-    console.log("app is listening at port 8080");
+    console.log("App is listening at port 8080");
 });
